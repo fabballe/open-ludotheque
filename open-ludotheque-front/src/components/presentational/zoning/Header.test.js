ecde5eb71824dc2a34031bf1055cece6587cbfb3
Fabien ABBALLE <fabien.abballe@trsb.net>
2017-05-29 11:46:36 +0200

    ajout TU

--- open-ludotheque-front/src/components/presentational/zoning/Header.test.js
+++ open-ludotheque-front/src/components/presentational/zoning/Header.test.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { shallow, mount } from 'enzyme';
+import { Link, MemoryRouter } from 'react-router-dom';
+
 import Header  from './Header.js';
+
 
 describe('<Header />', () => {
 
@@ -10,13 +13,18 @@ describe('<Header />', () => {
       ReactDOM.render(<Header onClick={() => {}} isAuthenticated={true} />, div);
     });
 
-    it('renders with good authentication link', () => {
+    it('renders with disconnect link', () => {
         const wrapper = mount(<Header onClick={() => {}} isAuthenticated={true} />);
-        const authenticatedLink = <a href="#">Se déconnecter</a>;
-        //console.log(wrapper);
-        //console.log(authenticatedLink);
-        console.log(wrapper.html());
+        const disconnectLink = <a href="#">Se déconnecter</a>;
         expect(wrapper.props().isAuthenticated).toEqual(true);
-        expect(wrapper.contains(authenticatedLink)).toEqual(true);
+        expect(wrapper.containsMatchingElement(disconnectLink)).toEqual(true);
+    });
+
+    it('renders with connect link', () => {
+        const wrapper = mount(<MemoryRouter><Header onClick={() => {}} isAuthenticated={false} /></MemoryRouter>);
+        const authenticatedLink = <Link to="/login">Se connecter</Link>;
+        const headerComponent = wrapper.find(Header);
+        expect(headerComponent.props().isAuthenticated).toEqual(false);
+        expect(headerComponent.containsMatchingElement(authenticatedLink)).toEqual(true);
     });
 });
